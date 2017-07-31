765fff4bc6d68d471001c472eeac411753286453
jlooper <jen.looper@gmail.com>
2017-07-31 15:23:16 -0400

    first pass at swipable cards

--- app/shared/view-models/match-view-model.js
+++ app/shared/view-models/match-view-model.js
@@ -1,4 +1,8 @@
 const observableModule = require("data/observable");
+const GridLayout = require("tns-core-modules/ui/layouts/grid-layout");
+const ItemSpec = require("tns-core-modules/ui/layouts/grid-layout");
+const Layout = require("tns-core-modules/ui/layouts/layout");
+var matches = new MatchViewModel();
 
 function MatchViewModel(petModel) {
     const viewModel = observableModule.fromObject({
--- app/views/match/match.js
+++ app/views/match/match.js
@@ -1,8 +1,9 @@
 const topmost = require("ui/frame").topmost;
-
+var SwipeEvent = require('nativescript-swipe-card');
 var MatchViewModel = require("../../shared/view-models/match-view-model");
 
 exports.onNavigatingTo = function(args) {
     const page = args.object;
     page.bindingContext = new MatchViewModel(page.navigationContext);
-};
+    //MatchViewModel.loadMatches();
+}
