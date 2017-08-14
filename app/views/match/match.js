be5282876c04a196ad50bde7c58ba26cb52bfb1c
Luna Kang <luna.kang.ca@gmail.com>
2017-08-14 11:29:59 -0700

    Start populating cards

--- app/views/match/match.js
+++ app/views/match/match.js
@@ -2,10 +2,19 @@ const topmost = require("ui/frame").topmost;
 var SwipeEvent = require('nativescript-swipe-card');
 
 const MatchViewModel = require("../../shared/view-models/match-view-model");
+var match = new MatchViewModel();
 
 exports.onNavigatingTo = function(args) {
     const page = args.object;
     page.bindingContext = new MatchViewModel(page.navigationContext);
     const binding = page.bindingContext;
     binding.getMatches(binding.pet.zip);
+    let swipeCard = page.getViewById("swipe");
+    // swipeCard.on("swipeEvent", (args:SwipeEvent) => {
+    //     if (args.direction === 1) {
+    //         console.log('Swiped to right');
+    //     } else {
+    //         console.log('Swiped to left');
+    //     }
+    // });
 }
