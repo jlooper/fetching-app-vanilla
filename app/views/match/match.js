3d8c4630e8f4bb60b3ca4924be5df40845c0fcd1
jlooper <jen.looper@gmail.com>
2017-08-18 23:05:14 +0200

    ermahgerd it's werkin'!!!!

--- app/views/match/match.js
+++ app/views/match/match.js
@@ -1,32 +1,14 @@
 const topmost = require("ui/frame").topmost;
 var SwipeEvent = require("nativescript-swipe-card");
-
 var MatchViewModel = require("../../shared/view-models/match-view-model");
-var match = new MatchViewModel();
-var cards; 
+
 
 exports.onNavigatingTo = function(args) {
   const page = args.object;
-  debugger;
   page.bindingContext = new MatchViewModel(page.navigationContext);
   const binding = page.bindingContext;
-  getMatches(binding.pet.zip);
-
-  //let swipeCard = page.getViewById("swipe");
-  // swipeCard.on("swipeEvent", (args:SwipeEvent) => {
-  //     if (args.direction === 1) {
-  //         console.log('Swiped to right');
-  //     } else {
-  //         console.log('Swiped to left');
-  //     }
-  // });
-};
-
-getMatches = function(zip) {
-    match.getMatches(zip).then(
-    function() {
-      console.log(match.petCards)
-    },
+  binding.getMatches(binding.pet.zip).then(
+    function() {},
     function(err) {
       console.log("getMatches error", err);
     }
