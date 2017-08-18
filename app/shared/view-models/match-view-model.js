78dd2bd88db1a811105b15661c7871e15bc39e3c
jlooper <jen.looper@gmail.com>
2017-08-18 10:33:26 +0200

    looping and creation of StackLayout for cards

--- app/shared/view-models/match-view-model.js
+++ app/shared/view-models/match-view-model.js
@@ -2,7 +2,7 @@ const observableModule = require("data/observable");
 var StackLayout = require("ui/layouts/stack-layout").StackLayout;
 var Label = require("tns-core-modules/ui/label").Label;
 var config = require("../../shared/config");
-
+var ImageModule = require("tns-core-modules/ui/image");
 
 
 function MatchViewModel(petModel) {
@@ -16,39 +16,27 @@ function MatchViewModel(petModel) {
             console.log("getting matches")
             
             var petCards = [];
-
-            //hard-coding isn't happy either
-            /*var stack1 = new StackLayout();
-            var label1 = new Label();
-            label1.text="one";
-            stack1.verticalAlignment = "middle";
-            stack1.addChild(label1);
-
-            var stack2 = new StackLayout();
-            var label2 = new Label();
-            label2.text="two";
-            stack2.verticalAlignment = "middle";
-            stack2.addChild(label2);
-
-            petCards = [stack1,stack2];
-            viewModel.petCards = petCards; */
             
             return fetch(config.apiUrl + 'pet.find?&key='+ config.apiKey + '&location=' + zip + '&animal=dog&output=basic&count=10&format=json')
                 .then(handleErrors)
                 .then(function(response) {
                     return response.json();
                 }).then(function(data) {
-                console.log(JSON.stringify(data))
-                
-                //build card - this needs to loop
-                var stack = new StackLayout();
-                var image = new Image();
-                image.src=data.petfinder.pet.media.photos.photo[3].$t;
-                image.height=100;
-                image.width=100;
-                stack.verticalAlignment = "middle";
-                stack.addChild(image);
-                petCards = [stack];
+                //console.log(JSON.stringify(data.petfinder.pets))
+                //build card - this needs to loop through data.petfinder.pets
+                var arrayLength = data.petfinder.pets.pet.length;
+                console.log(arrayLength)
+                for (var i = 0; i < arrayLength; i++) {
+                    var stack = new StackLayout();
+                    var image = new ImageModule.Image();
+                    image.src=data.petfinder.pets.pet[i].media.photos.photo[3].$t;
+                    console.log(image.src)
+                    image.height=100;
+                    image.width=100;
+                    stack.verticalAlignment = "middle";
+                    stack.addChild(image);
+                    petCards.push(stack);
+                }
                 viewModel.petCards = petCards;            
             });
             
