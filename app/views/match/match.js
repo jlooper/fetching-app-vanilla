2d8b73b72f7687a901d8480d72fe207f6458b7c0
Brad Martin <bradwaynemartin@gmail.com>
2017-08-17 23:54:43 -0500

    promise ya the world

--- app/views/match/match.js
+++ app/views/match/match.js
@@ -1,27 +1,39 @@
 const topmost = require("ui/frame").topmost;
-var SwipeEvent = require('nativescript-swipe-card');
+var SwipeEvent = require("nativescript-swipe-card");
 
 var MatchViewModel = require("../../shared/view-models/match-view-model");
-var match = new MatchViewModel ();
+var match = new MatchViewModel();
 
 exports.onNavigatingTo = function(args) {
-    const page = args.object;
-    page.bindingContext = new MatchViewModel(page.navigationContext);
-    const binding = page.bindingContext;
-    getMatches(binding.pet.zip);
-   
-    //let swipeCard = page.getViewById("swipe");
-    // swipeCard.on("swipeEvent", (args:SwipeEvent) => {
-    //     if (args.direction === 1) {
-    //         console.log('Swiped to right');
-    //     } else {
-    //         console.log('Swiped to left');
-    //     }
-    // });
-}
+  const page = args.object;
+  debugger;
+  page.bindingContext = new MatchViewModel(page.navigationContext);
+
+  console.log(JSON.stringify(page.navigationContext));
+
+  const binding = page.bindingContext;
+  getMatches(binding.pet.zip);
+
+  //let swipeCard = page.getViewById("swipe");
+  // swipeCard.on("swipeEvent", (args:SwipeEvent) => {
+  //     if (args.direction === 1) {
+  //         console.log('Swiped to right');
+  //     } else {
+  //         console.log('Swiped to left');
+  //     }
+  // });
+};
 
 getMatches = function(zip) {
-    match.getMatches(zip)
-    var petCards = match.petCards;
-    console.log(match.petCards)
-}
+  //   match.getMatches(zip);
+  // get matches returns a fetch promise
+  match.getMatches(zip).then(
+    function() {
+      var petCards = match.petCards;
+      console.log(match.petCards);
+    },
+    function(err) {
+      console.log("getMatches error", err);
+    }
+  );
+};
--- app/views/pet/pet.js
+++ app/views/pet/pet.js
@@ -1,42 +1,44 @@
 var PetViewModel = require("../../shared/view-models/pet-view-model");
-var observableModule = require("data/observable")
+var observableModule = require("data/observable");
 var ObservableArray = require("data/observable-array").ObservableArray;
 var page;
 var pet = new PetViewModel();
 const topmost = require("ui/frame").topmost;
 
-
 exports.navigated = function(args) {
-    page = args.object;
-    page.bindingContext = new PetViewModel(page.navigationContext);
+  page = args.object;
+  page.bindingContext = new PetViewModel(page.navigationContext);
 
-    pickPet();
+  pickPet();
 };
 
-exports.onRefresh = function(){
-    pickPet();
-}
+exports.onRefresh = function() {
+  pickPet();
+};
 
 exports.match = function(args) {
-    topmost().navigate({
-        moduleName: "views/match/match",
-        context: pet,
-        animated: true,
-        transition: {
-            name: "slide",
-            duration: 200,
-            curve: "ease"
-        }
-    });
+  topmost().navigate({
+    moduleName: "views/match/match",
+    context: pet,
+    animated: true,
+    transition: {
+      name: "slide",
+      duration: 200,
+      curve: "ease"
+    }
+  });
 };
 
-function pickPet(){
-    
-    pet.empty()
-    pet.load()
-    .catch(function(error){
-        return Promise.reject();
-    }).then(function(){
-        page.bindingContext = pet;
+function pickPet() {
+  pet.empty();
+  pet
+    .load()
+    .catch(function(error) {
+      /// error thrown
+      console.log(error);
+      return Promise.reject();
+    })
+    .then(function() {
+      page.bindingContext = pet;
     });
 }
